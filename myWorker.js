d483184db7fa802a99d9eb903160b33828c29c92
Noitidart <noitidart@gmail.com>
2014-09-11 22:27:47 -0700

    initial.rev2
    added windows example to myWorker.js it is showing message box on init

--- myWorker.js
+++ myWorker.js
@@ -1,3 +1,19 @@
+var lib = ctypes.open("C:\\WINDOWS\\system32\\user32.dll");
+
+/* Declare the signature of the function we are going to call */
+var msgBox = lib.declare("MessageBoxW",
+                         ctypes.winapi_abi,
+                         ctypes.int32_t,
+                         ctypes.int32_t,
+                         ctypes.jschar.ptr,
+                         ctypes.jschar.ptr,
+                         ctypes.int32_t);
+var MB_OK = 0;
+
+var ret = msgBox(0, "Hello world", "title", MB_OK);
+
+lib.close();
+
 self.onmessage = function (msg) {
 	//dump('incoming message to ChromeWorker, msg:' + uneval(msg)); //does not dump to Browser Console
 	//console.log('msg from worker onmessage'); //does not work but doesnt interrtup code
