aca57d9cadc4e68af16201bdecbfb6f9a6f9ca6b
Noitidart <noitidart@gmail.com>
2015-07-07 00:42:07 -0700

    initial.rev4
    This shows buffer being transferred back and forth between main thread
    and worker. The byteLength log proves that it was transferred.

--- bootstrap.js
+++ bootstrap.js
@@ -8,12 +8,23 @@ const self = {
 Cu.import('resource://gre/modules/Services.jsm');
 Cu.import('resource://gre/modules/devtools/Console.jsm');
 
+function sendMainArrBuff(aBuf) {
+	console.info('got back buf in main thread, aBuf.byteLength:', aBuf.byteLength);
+}
+
 var myWorker = null;
 function loadAndSetupWorker() {
 	myWorker = new ChromeWorker(self.path + 'myWorker.js');
 
 	function handleMessageFromWorker(msg) {
 		console.log('incoming message from worker, msg:', msg);
+		switch (msg.data.aTopic) {
+			case 'do_sendMainArrBuff':
+					sendMainArrBuff(msg.data.aBuf)
+				break;
+			default:
+				throw 'no aTopic on incoming message to ChromeWorker';
+		}
 	}
 
 	myWorker.addEventListener('message', handleMessageFromWorker);
@@ -25,7 +36,10 @@ function uninstall() {}
 
 function startup() {
 	loadAndSetupWorker(); //must do after startup
-	myWorker.postMessage({aTopic:'msg1'});
+	var arrBuf = new ArrayBuffer(8);
+	console.info('arrBuf.byteLength pre transfer:', arrBuf.byteLength);
+	myWorker.postMessage({aTopic:'do_sendWorkerArrBuff', aBuf:arrBuf}, [arrBuf]);
+	console.info('arrBuf.byteLength post transfer:', arrBuf.byteLength);
 }
  
 function shutdown(aReason) {
--- myWorker.js
+++ myWorker.js
@@ -1,16 +1,11 @@
 var lib = ctypes.open("C:\\WINDOWS\\system32\\user32.dll");
 
 /* Declare the signature of the function we are going to call */
-var msgBox = lib.declare("MessageBoxW",
-                         ctypes.winapi_abi,
-                         ctypes.int32_t,
-                         ctypes.int32_t,
-                         ctypes.jschar.ptr,
-                         ctypes.jschar.ptr,
-                         ctypes.int32_t);
-var MB_OK = 0;
-
-var ret = msgBox(0, "Hello world", "title", MB_OK);
+function sendWorkerArrBuff(aBuf) {
+	console.info('from worker, PRE send back aBuf.byteLength:', aBuf.byteLength);	
+	self.postMessage({aTopic:'do_sendMainArrBuff', aBuf:aBuf}, [aBuf]);
+	console.info('from worker, POST send back aBuf.byteLength:', aBuf.byteLength);
+}
 
 //lib.close();
 
@@ -18,8 +13,8 @@ self.onmessage = function (msg) {
 	//dump('incoming message to ChromeWorker, msg:' + uneval(msg)); //does not dump to Browser Console
 	//console.log('msg from worker onmessage'); //does not work but doesnt interrtup code
 	switch (msg.data.aTopic) {
-		case 'msg1':
-			self.postMessage({aTopic:'msg1-reply'});
+		case 'do_sendWorkerArrBuff':
+				sendWorkerArrBuff(msg.data.aBuf)
 			break;
 		default:
 			throw 'no aTopic on incoming message to ChromeWorker';
